ebdc6a662e864de4b33af23063d8bcefe7bd84f0
Prince Vishwakarma <pko62547@gmail.com>
2025-07-29 17:20:43 +0530

    add not-found and signup page

---
+++ chat-ai/src/pages/NotFound.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Box } from "@mui/material";
+
+const NotFound = () => {
+  return (
+    <Box
+      sx={{
+        mt: "200px",
+        textAlign: "center",
+      }}
+    >
+      <h1>Page Not Found 😒</h1>
+    </Box>
+  );
+};
+
+export default NotFound;
